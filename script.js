8de22f040905777bbad45305fd97ac82d784fee2
POTHULA. JAI SAI <92910772+jaisaipothula@users.noreply.github.com>
2024-07-16 21:05:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,9 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const list = document.getElementById('list');
+() => { 
+    cy.visit("http://localhost:3000");
 
-    // Function to add a specified number of list items
-    function addItems(numItems) {
-        for (let i = 1; i <= numItems; i++) {
-            const li = document.createElement('li');
-            li.textContent = `List item ${i}`;
-            list.appendChild(li);
-        }
-    }
+    // Wait for the element #infi-list to exist and be visible
+    cy.get('#infi-list', { timeout: 10000 }).should('exist').scrollTo('bottom');
+}
 
-    // Add 10 list items initially
-    addItems(10);
-
-    // Function to add more items when scrolled to the end
-    function addMoreItems() {
-        // Add 2 more items
-        addItems(2);
-    }
-
-    // Detect when user scrolls to the end of the list
-    list.addEventListener('scroll', function() {
-        if (list.scrollTop + list.clientHeight >= list.scrollHeight) {
-            // Reached the bottom of the list
-            addMoreItems();
-        }
-    });
-});
 
 
